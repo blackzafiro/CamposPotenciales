bce5e307eef7b3f54289eedb5d195172e6e2e9da
blackzafiro <blackzafiro.net@gmail.com>
2019-04-10 15:45:20 -0500

    Un marcador indica la velocidad lineal del robot.

--- src/occupancy_grid_map.cpp
+++ src/occupancy_grid_map.cpp
@@ -4,6 +4,7 @@
 #include <visualization_msgs/Marker.h>
 #include <nav_msgs/OccupancyGrid.h>
 #include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/Twist.h>
 #include <vector>
 // %EndTag(INCLUDES)%
 
@@ -12,13 +13,16 @@
 /// El eje Y es verde.
 /// El eje Z apunta hacia arriba y el marcador es azul.
 
-const int WIDTH = 24;       /// A lo largo del eje rojo x
-const int HEIGHT = 31;      /// A lo largo del eje verde
-const float RESOLUTION = 0.3f; /// [m/cell]
-
 class Mapa {
 private:
+  const int WIDTH = 24;          /// A lo largo del eje rojo x
+  const int HEIGHT = 31;         /// A lo largo del eje verde
+  const float RESOLUTION = 0.3f; /// [m/cell]
+
   ros::Publisher marker_pub;
+  visualization_msgs::Marker velocity_mark;
+
+  ros::Publisher grid_pub;
   nav_msgs::OccupancyGrid map;
 
   ros::NodeHandle& r_n;
@@ -28,16 +32,49 @@ public:
   /** Constructor. */
   Mapa(ros::NodeHandle& r_n) : r_n(r_n)
   {
-    marker_pub = r_n.advertise<nav_msgs::OccupancyGrid>("occupancy_marker", 1);
+    marker_pub = r_n.advertise<visualization_msgs::Marker>("visualization_marker", 1);
+    grid_pub = r_n.advertise<nav_msgs::OccupancyGrid>("occupancy_marker", 1);
+    llenaVelocidad();
     llenaMapa();
+  }
+
+  /** Recibe la velocidad en coordenadas del robot y publica para rviz. */
+  void publicaVelocidad(const geometry_msgs::Twist& robotVel)
+  {
+    velocity_mark.scale.x = robotVel.linear.x;
+    marker_pub.publish(velocity_mark);
   }
 
   void publicate()
   {
-    marker_pub.publish(map);
+    grid_pub.publish(map);
   }
 
 private:
+  void llenaVelocidad()
+  {
+    velocity_mark.header.frame_id = "base_link";
+    velocity_mark.header.stamp = ros::Time();
+    velocity_mark.ns = "kobu_namespace";
+    velocity_mark.id = 0;
+    velocity_mark.type = visualization_msgs::Marker::ARROW;
+    velocity_mark.action = visualization_msgs::Marker::ADD;
+    velocity_mark.pose.position.x = 0;
+    velocity_mark.pose.position.y = 0;
+    velocity_mark.pose.position.z = 0.2;
+    velocity_mark.pose.orientation.x = 0.0;
+    velocity_mark.pose.orientation.y = 0.0;
+    velocity_mark.pose.orientation.z = 0.0;
+    velocity_mark.pose.orientation.w = 1.0;
+    velocity_mark.scale.x = 0.5;
+    velocity_mark.scale.y = 0.05;
+    velocity_mark.scale.z = 0.05;
+    velocity_mark.color.a = 1.0; // Don't forget to set the alpha!
+    velocity_mark.color.r = 0.0;
+    velocity_mark.color.g = 1.0;
+    velocity_mark.color.b = 0.0;
+  }
+
   /** Agrega los obstáculos al mapa */
   void llenaMapa()
   {
@@ -65,13 +102,18 @@ private:
       data[i] = 0;
     }
 
-    data[0] = 50;                            // El origen está en la esquina inferior izquierda.
+    //data[0] = 50;                               // El origen está en la esquina inferior izquierda.
     fillRectangle(data, 0, 1, 0, WIDTH-1, 100);   // Renglón 0. Las columnas van de 0 a WIDTH-1.  Los renglones corren sobre el eje Y.
-    fillRectangle(data, 1, 0, HEIGHT-1, 0, 100);  // Columna 0. Los renglones va de 0 a HEIGHT-1.  Las columnas corren sobre el eje X.
-    
-    //fillRectangle(data, 0, 0, 2, 2);
-    //data[5*WIDTH+3] = 100;
-    //data[5*WIDTH+2] = -1;
+    fillRectangle(data, 0, 0, HEIGHT-1, 0, 100);  // Columna 0. Los renglones va de 0 a HEIGHT-1.  Las columnas corren sobre el eje X.
+    fillRectangle(data, HEIGHT-1, 1, HEIGHT-1, WIDTH-1, 100);
+    fillRectangle(data, 1, WIDTH-1, HEIGHT-1, WIDTH-1, 100);
+    fillRectangle(data, 5, 1, 6, 11, 100);          // Mesa izq1
+    fillRectangle(data, 11, 1, 13, 11, 100);        // Mesa izq2
+    fillRectangle(data, 18, 1, 20, 11, 100);        // Mesa izq3
+    fillRectangle(data, 5, 17, 6, 22, 100);          // Mesa der1
+    fillRectangle(data, 11, 17, 13, 22, 100);          // Mesa der2
+    fillRectangle(data, 18, 17, 20, 22, 100);          // Mesa der3
+
     map.data = std::vector<int8_t>(data, data + size);
   
     // %EndTag(MAP_INIT)%
@@ -108,6 +150,7 @@ void receiveNavGoal(const geometry_msgs::PoseStamped& poseStamped)
 }
 
 
+
 // %Tag(INIT)%
 int main( int argc, char** argv )
 {
@@ -117,6 +160,7 @@ int main( int argc, char** argv )
   //ros::Publisher marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 1);
   Mapa mapa(n);
   ros::Subscriber sub = n.subscribe("/move_base_simple/goal", 5, receiveNavGoal); // Máximo 5 mensajes en la cola.
+  ros::Subscriber sub_vel = n.subscribe("/mobile_base/commands/velocity", 2, &Mapa::publicaVelocidad, &mapa); // Máximo 5 mensajes en la cola.
 // %EndTag(INIT)%
 
 
